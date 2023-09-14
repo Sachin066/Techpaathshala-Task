926e908a330261253d048a82fd9d98995952fcc9
sachin saroj <91935812+Sachin066@users.noreply.github.com>
2023-09-14 05:13:27 +0000

    Update 4 files

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import './header.css';
-import netflix from 'D:/React Js Projects/techpaathshala-task/src/images/netflix.png'
-function Nav(){
-    return(
-
-
-        <div>
+import React from "react";
+import "./header.css";
+// import netflix from "../header/netflix";
+function Nav() {
+  return (
+    <div>
       <header>
         <nav>
-            <img src={netflix} alt="netflix" className='image'/>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/2504/2504929.png"
+            alt="netflix"
+            className="image"
+          />
 
-            <ul>
-                <li className="bar"><a href="#">HOME</a></li>
-                <li className="bar"><a href="#">TV SHOWS</a></li>
-                <li className="bar"><a href="#">MOVIES</a></li>
-                <li className="bar"><a href="#">LETEST</a></li>
-                <li className="bar"><a href="#">MYLIST</a></li>
-            </ul>
+          <ul>
+            <li className="bar">
+              <a href="#">HOME</a>
+            </li>
+            <li className="bar">
+              <a href="#">TV SHOWS</a>
+            </li>
+            <li className="bar">
+              <a href="#">MOVIES</a>
+            </li>
+            <li className="bar">
+              <a href="#">LETEST</a>
+            </li>
+            <li className="bar">
+              <a href="#">MYLIST</a>
+            </li>
+          </ul>
         </nav>
       </header>
-
     </div>
-    );
+  );
 }
 export default Nav;
